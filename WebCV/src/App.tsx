ea871a82af353b76d216d87ef9d95f14510a11a3
FrancoPela <fran080j9@gmail.com>
2023-06-03 16:01:59 -0300

    1

--- WebCV/src/App.tsx
+++ WebCV/src/App.tsx
@@ -4,9 +4,22 @@ function App() {
 
 
   return (
-    <>
-    <h1 className=''>1</h1>
-    </>
+    <div className='h-full w-full'>
+
+    <div className='flex-col'>
+      <div className='flex h-96 w-3/5 bg-programming rounded-br-md items-center'>
+        
+        <div className='m-11 flex-row max-h-28 max-w-sm'>
+          <p className='text-left text-5xl'>Hi! Im Franco. </p>
+          <p className='text-left text-3xl'>Software Developer</p>
+        </div>
+        <div>
+          <p className='text-left text-1xl'>Im a young software developer looking to gain exprience in proyects </p>
+        </div>
+      </div>
+    </div>
+   
+    </div>
   )
 }
 
