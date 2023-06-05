de196e07b61fbfaf8cf023924278f298e0a05ff1
FrancoPela <fran080j9@gmail.com>
2023-06-05 13:51:50 -0300

    1

--- WebCV/src/App.tsx
+++ WebCV/src/App.tsx
@@ -1,25 +1,19 @@
 import './App.css'
 
+
 function App() {
 
 
   return (
-    <div className='h-full w-full'>
+    <div>
+      <div>
+        <h1>hola</h1>
 
-    <div className='flex-col'>
-      <div className='flex h-96 w-3/5 bg-programming rounded-br-md items-center'>
-        
-        <div className='m-11 flex-row max-h-28 max-w-sm'>
-          <p className='text-left text-5xl'>Hi! Im Franco. </p>
-          <p className='text-left text-3xl'>Software Developer</p>
-        </div>
-        <div>
-          <p className='text-left text-1xl'>Im a young software developer looking to gain exprience in proyects </p>
-        </div>
       </div>
+
     </div>
    
-    </div>
+
   )
 }
 
---
+++ WebCV/src/Components/navbar.tsx
@@ -0,0 +1,13 @@
+import "./navbar.css"
+
+function Navbar() {
+    return(
+        <header >
+        <div className="w-full h-[30px] bg-[]">
+            <h1>Franco Lago</h1>
+        </div>
+        </header>
+    )
+}
+
+export default Navbar
