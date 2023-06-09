a3b53e0600ada89aa68cff5e1ee76c3cc8fa43d3
FrancoPela <fran080j9@gmail.com>
2023-06-09 15:07:34 -0300

    1

--- WebCV/src/App.tsx
+++ WebCV/src/App.tsx
@@ -9,12 +9,14 @@ function App() {
       <div>
 
         <div className='\\cards container\\ flex flex-col w-full h-[750px] items-center gap-10'>
-          <div className='\\title\\ flex-row mt-6 ml-5 self-start'>
+
+          <div className='\\title\\ mt-4 ml-5 self-start'>
             <p className='text-[40px]'>{">"}Franco Lago</p>
             <p className='text-[30px]'>Software Developer;</p>
           </div>
           <div className='\\about me\\ h-[284px] w-[350px] bg-white shadow-xl '>
-            <div className='\\header\\ flex flex-row mt-8 ml-7'>
+
+            <div className='\\header\\ flex flex-row mt-5 ml-7'>
               <div className='\\img\\ rounded-[200px] h-[80px] w-[80px] bg-myface'></div>
               <div className='\\title\\ mt-3 ml-3'>
                 <p className='text-[12px]'>{"#Include <presentation>"}</p>
@@ -22,8 +24,16 @@ function App() {
               </div>
             </div>
 
+            <p className='text-[12px] ml-8 mt-3 max-w-[300px]'>{">"}Im a young computer science student from argentina looking to expand my knoledge and to gain experience by working in challenging and amazing projects.</p>
+            <p className='text-[12px] ml-8 mt-3 max-w-[300px]'>{">"}Im a very comunicative person and i like working with other people, im open minded and i enjoy learning new things.</p>
           </div>
+
+
           <div className='\\knoledges\\ h-[300px] w-[350px] bg-white shadow-xl '>
+            <div className='\\title\\ mt-6 ml-5'>
+              <p className='text-[12px] ml-8'>{"fopen(“my_knoledges.txt”, “r”);"}</p>
+              <p className='text-[30px]'>{">>"}My knoledges</p>
+            </div>
           </div>
         </div>
 
