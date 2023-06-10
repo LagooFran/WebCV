088b872731cce5776902465de04ea41cf620e805
FrancoPela <fran080j9@gmail.com>
2023-06-10 13:46:32 -0300

    1

--- WebCV/src/App.tsx
+++ WebCV/src/App.tsx
@@ -7,11 +7,23 @@ import "./slick-theme.css";
 
 function App() {
   const settings = {
+    autoplay: true,
+    autoplaySpeed: 2000,
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 2,
+  };
+
+  const settings2 = {
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
   return (
@@ -24,7 +36,9 @@ function App() {
           </div>
           <div className="\\about me\\ h-[284px] w-[350px] bg-white shadow-xl ">
             <div className="\\header\\ flex flex-row mt-5 ml-7">
-              <div className="\\img\\ rounded-[200px] h-[80px] w-[80px] bg-myface"></div>
+              <div className="\\img\\ rounded-[200px] h-[80px] w-[80px]">
+                <img className="rounded-[200px]" src="/myface.jpeg" alt="" />
+              </div>
               <div className="\\title\\ mt-3 ml-3">
                 <p className="text-[12px]">{"#Include <presentation>"}</p>
                 <p className="text-[30px]">{">>"}About Me</p>
@@ -120,7 +134,7 @@ function App() {
                           <p className="ml-2">Sql Server</p>
                         </div>
                       </div>
-                      
+
                       <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
                         <div className="flex flex-row items-center">
                           <div className="h-[60px] w-[60px] flex">
@@ -166,9 +180,96 @@ function App() {
                       <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
                         <div className="flex flex-row items-center">
                           <div className="h-[60px] w-[60px] flex">
-                            <img src="/icons/js.png" alt="" />
+                            <img src="/icons/react.png" alt="" />
                           </div>
-                          <p className="ml-2">Javascript</p>
+                          <p className="ml-2">React</p>
+                        </div>
+                      </div>
+
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/asp.png" alt="" />
+                          </div>
+                          <p className="ml-2">ASP.net</p>
+                        </div>
+                      </div>
+
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/html-5.png" alt="" />
+                          </div>
+                          <p className="ml-2">HTML 5</p>
+                        </div>
+                      </div>
+
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/css-3.png" alt="" />
+                          </div>
+                          <p className="ml-2">CSS 3</p>
+                        </div>
+                      </div>
+                    </Slider>
+                  </div>
+                </TabPanel>
+
+                <TabPanel>
+                  <div className="\\sliderContainer\\ ml-2">
+                    <Slider {...settings}>
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/uml.png" alt="" />
+                          </div>
+                          <p className="ml-2">UML</p>
+                        </div>
+                      </div>
+
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/agile.png" alt="" />
+                          </div>
+                          <p className="ml-2">Agile</p>
+                        </div>
+                      </div>
+
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/xls.png" alt="" />
+                          </div>
+                          <p className="ml-2">Excel</p>
+                        </div>
+                      </div>
+
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/github.png" alt="" />
+                          </div>
+                          <p className="ml-2">Github</p>
+                        </div>
+                      </div>
+
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/figma.png" alt="" />
+                          </div>
+                          <p className="ml-2">Figma</p>
+                        </div>
+                      </div>
+
+                      <div className="h-[80px] max-w-[150px] flex flex-row mt-6">
+                        <div className="flex flex-row items-center">
+                          <div className="h-[60px] w-[60px] flex">
+                            <img src="/icons/adobe-photoshop.png" alt="" />
+                          </div>
+                          <p className="ml-2">Photoshop</p>
                         </div>
                       </div>
                     </Slider>
@@ -176,6 +277,29 @@ function App() {
                 </TabPanel>
               </Tabs>
             </div>
+          </div>
+        </div>
+
+        <div className="\\title\\ mt-8 ml-5 mb-10 self-start">
+          <p className="text-[40px]">{">"}Projects</p>
+        </div>
+        <div className="flex w-[100%] h-[200px] justify-center">
+          <div className="\\slider-container\\ w-[320px] h-[200px] mb-12 ">
+            <Slider {...settings2}>
+            <div className="flex h-[180px] max-w-[95%] bg-white">
+                <div className="h-[100px] w-[100%] flex">
+                  <img className="w-[100%]" src="/programming.jpg" alt=""></img>
+                </div>
+                <p className="ml-3 mt-1 text-[20px]">Color changer</p>
+              </div>
+
+              <div className="flex  h-[180px] max-w-[95%] bg-white">
+                <div className="h-[100px] w-[100%] flex">
+                  <img className="w-[100%]" src="/programming.jpg" alt=""></img>
+                </div>
+                <p className="ml-3 mt-1 text-[20px]">More On Github</p>
+              </div>
+            </Slider>
           </div>
         </div>
       </div>
--- WebCV/tailwind.config.js
+++ WebCV/tailwind.config.js
@@ -6,15 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'programming': "url('/programming.jpg')",
-        'myface': "url('/myface.jpeg')",
-        'cSharp': "url('/c-sharp.png')",
-        'c': "url('/icons/c.png')",
-        'c+': "url('/c+.png')",
-        'js': "url('/js.png')",
-        'python': "url('/python.png')"
-      }
     },
   },
   plugins: [],
